833dc2464763acd46eb5b4d7b513956b108083e8
Julian <julianibibo@gmail.com>
2020-09-08 22:36:51 +0200

    EH updated

--- src/com/steve/Const.java
+++ src/com/steve/Const.java
@@ -1,5 +1,7 @@
 package com.steve;
 
+import org.bukkit.World;
+import org.bukkit.entity.FallingBlock;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -26,7 +28,7 @@ public class Const {
                 JSONObject json = (JSONObject) playerObject;
 
                 HashMap<String, Object> data = new HashMap<>();
-                data.put("wins", (long) json.get("wins"));
+                data.put("wins", json.get("wins"));
 
                 playerData.put((String) json.get("name"), data);
             }
@@ -34,6 +36,19 @@ public class Const {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    static void editPlayerData(String username, String key, Object value) {
+        playerData.get(username).put(key, value);
+    }
+
+    static void incrementPlayerData(String username, String key) {
+
+    }
+
+    static void incrementPlayerData(String username, String key, long amount) {
+        long oldValue = (long) playerData.get(username).get(key);
+        long newValue = oldValue + amount;
+        playerData.get(username).put(key, newValue);
     }
 }
--- src/com/steve/EventListener.java
+++ src/com/steve/EventListener.java
@@ -1,21 +1,25 @@
 package com.steve;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
-import org.bukkit.Location;
+import com.steve.Commands.Steve;
+import org.bukkit.*;
 import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
 
 public class EventListener implements Listener {
 
+    JavaPlugin main;
+
     public EventListener(Main main) {
+        this.main = main;
         main.getServer().getPluginManager().registerEvents(this, main);
     }
 
@@ -27,17 +31,19 @@ public class EventListener implements Listener {
         GameMode gm = p.getGameMode();
         e.setCancelled(true);
 
-        String statusPrefix;
+        long wins = (long) Const.playerData.get(n).get("wins");
+
+        String prefix;
         if (gm == GameMode.SPECTATOR) {
-            statusPrefix = ChatColor.DARK_GRAY + "[S] ";
+            prefix = String.format(ChatColor.DARK_GRAY + "[S]" + ChatColor.UNDERLINE +"[%s]", wins);
         } else {
-            statusPrefix = ChatColor.GRAY + "";
+            prefix = String.format(ChatColor.GRAY + "[%s] ", wins);
         }
 
-        Bukkit.getLogger().info(String.format("%s%s > %s", statusPrefix, n, m));
+        Bukkit.getLogger().info(String.format("%s%s > %s", prefix, n, m));
 
         for (Player r : e.getRecipients()) {
-            r.sendMessage( String.format("%s%s > %s", statusPrefix, n, m));
+            r.sendMessage(String.format("%s%s > %s", prefix, n, m));
         }
     }
 
@@ -61,11 +67,13 @@ public class EventListener implements Listener {
     @EventHandler
     public void onPlayerWalkOnBlock(PlayerMoveEvent e){
         Location pos = e.getTo();
+        Player p = e.getPlayer();
 
         if(pos == null) return;
 
         Block b = pos.clone().subtract(0,1,0).getBlock();
         e.getPlayer().sendMessage("Block = " + b.getBlockData().getAsString());
+
     }
 
 }
