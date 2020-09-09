f0dcf1f10cceff9035b2573b7c1f39aecf92d9b0
Badr Ijaz <badrkmc@gmail.com>
2020-09-10 01:02:22 +0500

    Added socials and add friend

--- src/com/steve/Main.java
+++ src/com/steve/Main.java
@@ -2,6 +2,7 @@ package com.steve;
 
 import com.steve.Commands.GenerateMazeCommand;
 import com.steve.Commands.PlatformCommand;
+import com.steve.Commands.Social.AddFriend;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,11 +13,16 @@ public class Main extends JavaPlugin {
         PlayerData.readDisk();
         new EventListener(this);
 
-        // >spawnplatform
+        // > spawnPlatform
         getCommand("spawnplatform").setExecutor(new PlatformCommand());
 
-        // >generatemaze
+        // > generateMaze
         getCommand("generatemaze").setExecutor(new GenerateMazeCommand());
+
+
+        // > addFriend
+        getCommand("friend").setExecutor((new AddFriend()));
+
         Bukkit.getLogger().info("Enabled");
     }
 
