01bdebbbce4d88f7e306c24ab465232b4f2e38a9
Daan Ruting <daan.j.ruting@gmail.com>
2020-09-10 17:16:58 +0200

    Fixed amount of waypoints it should spawn instead waypoints -1

--- src/com/steve/Maze.java
+++ src/com/steve/Maze.java
@@ -1,8 +1,9 @@
 package com.steve;
 
+import org.bukkit.util.Vector;
+
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Vector;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Maze {
@@ -10,6 +11,7 @@ public class Maze {
     public enum TILEROLE{
         ENTRANCE,
         WAYPOINT,
+        PATH,
         DEADEND,
         EXIT,
         EMPTY
@@ -35,7 +37,9 @@ public class Maze {
         generatedMaze = new ArrayList<>();
 
         for (int i = 0; i < width; i++) {
+
             ArrayList<TILEROLE> list = new ArrayList<>();
+
             for (int j = 0; j < length; j++) {
                 list.add(TILEROLE.EMPTY);
             }
@@ -46,42 +50,136 @@ public class Maze {
 
     public void generateMaze(){
 
+        ArrayList<Vector> entranceList  = new ArrayList<>(),
+                exitList  = new ArrayList<>(),
+                deadendList  = new ArrayList<>(),
+                waypointList = new ArrayList<>();
+
         //Set entrances
-        ArrayList<TILEROLE> list = generatedMaze.get(0);
+
+        ArrayList<TILEROLE> row = generatedMaze.get(0);
+
         for(int i = 0; i < entrances;i++){
+
             int randomInt = ThreadLocalRandom.current().nextInt(0,width);
-            if(list.get(randomInt) != TILEROLE.ENTRANCE){
-                list.set(randomInt, TILEROLE.ENTRANCE);
+
+            if(row.get(randomInt) != TILEROLE.ENTRANCE){
+                row.set(randomInt, TILEROLE.ENTRANCE);
+                entranceList.add(new Vector().setX(0).setY(randomInt));
             }else{
-                list.set((randomInt+(width-1)/2)%width-1, TILEROLE.ENTRANCE);
+                row.set((randomInt+(width-1)/2)%width-1, TILEROLE.ENTRANCE);
+                entranceList.add(new Vector().setX(0).setY((randomInt+(width-1)/2)%width-1));
             }
         }
 
         //Set exits
-        list = generatedMaze.get(width-1);
+
+        row = generatedMaze.get(width-1);
+
         for(int i = 0; i < exits; i++){
-            int randomInt = ThreadLocalRandom.current().nextInt(0,width-1);
-            if(list.get(randomInt) != TILEROLE.EXIT){
-                list.set(randomInt, TILEROLE.EXIT);
+
+            int randomInt = ThreadLocalRandom.current().nextInt(0,width);
+
+            if(row.get(randomInt) != TILEROLE.EXIT){
+                row.set(randomInt, TILEROLE.EXIT);
+                exitList.add(new Vector().setX(width-1).setY(randomInt));
             }else{
-                list.set((randomInt+(width-1)/2)%width-1, TILEROLE.EXIT);
+                row.set((randomInt+(width-1)/2)%width-1, TILEROLE.EXIT);
+                exitList.add(new Vector().setX(width-1).setY((randomInt+(width-1)/2)%width-1));
             }
         }
-        //test
+
         //Set waypoints
+
         for(int i = 0; i < Math.max(3,Math.min(waypoints, (length-2)/2)); i++){
-            RecurseRandom(list);
+            RecurseRandomWaypoints(row);
+        }
+
+        //Set deadends
+
+        for(int i = 0; i < deadends; i++){
+            int randomIntWidth = ThreadLocalRandom.current().nextInt(0, width);
+            int randomIntLength = ThreadLocalRandom.current().nextInt(1, length-2);
+
+            row = generatedMaze.get(randomIntWidth);
+            if(row.get(randomIntLength) != TILEROLE.WAYPOINT){
+                row.set(randomIntLength, TILEROLE.DEADEND);
+                waypointList.add(new Vector().setX(randomIntWidth).setY(randomIntLength));
+            }
+        }
+
+        //Astar PathFinding
+        ArrayList<ArrayList<Integer>> pathfindGrid = new ArrayList<>();
+
+        //Initialising
+        for (int i = 0; i < width; i++) {
+            ArrayList<Integer> list = new ArrayList<>();
+
+            for (int j = 0; j < length; j++) {
+                list.add(0);
+            }
+
+            pathfindGrid.add(list);
+        }
+
+        Vector destination;
+        ArrayList<Vector> usedWaypoints = new ArrayList<>();
+        for (int i = 0; i < entrances; i++) {
+            //Get closest waypoint to route it towards
+            destination = nearestVector(entranceList.get(i),waypointList);
+            //Insert pathfinding function
+            routePathFinding(entranceList.get(i), destination);
+            usedWaypoints.add(destination);
+        }
+        for(Vector v: usedWaypoints){
+            waypointList.remove(v);
         }
     }
 
-    private void RecurseRandom(ArrayList<TILEROLE> list){
+    private Vector nearestVector(Vector v, ArrayList<Vector> vectors){
+
+        Vector closestVector = null;
+
+        for (Vector i: vectors) {
+            if(closestVector == null){
+                closestVector = i;
+                continue;
+            }
+            if(i.length() - v.length() < closestVector.length() - v.length()){
+                closestVector = i;
+            }
+        }
+
+        return v;
+    }
+
+    private void routePathFinding(Vector a, Vector b){
+
+        int level = (int)a.getY();
+        for (int i = (int) a.getX(); i < (int)b.getX(); i++) {
+            if(i == (int)b.getX()){
+                for (int j = level; j < (int)b.getY(); j++) {
+                    generatedMaze.get(i).set(level, TILEROLE.PATH);
+                }
+                return;
+            }
+            if(ThreadLocalRandom.current().nextInt(2) == 0 && level != (int)b.getY()){
+                level++;
+            }
+            generatedMaze.get(i).set(level, TILEROLE.PATH);
+        }
+    }
+
+    private void RecurseRandomWaypoints(ArrayList<TILEROLE> list){
+        int randomIntWidth = ThreadLocalRandom.current().nextInt(0, width);
         int randomIntLength = ThreadLocalRandom.current().nextInt(1, length-2);
-        int randomIntWidth = ThreadLocalRandom.current().nextInt(0, width-1);
+
         list = generatedMaze.get(randomIntLength);
+
         if(!list.contains(TILEROLE.WAYPOINT)){
             list.set(randomIntWidth, TILEROLE.WAYPOINT);
         }else{
-            RecurseRandom(list);
+            RecurseRandomWaypoints(list);
         }
     }
 
