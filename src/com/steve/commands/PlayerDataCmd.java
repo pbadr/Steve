cda5c47b9cd3d001f2a4501207e1970e5ccc4ff8
Julian <julianibibo@gmail.com>
2020-09-11 14:42:27 +0200

    unnec. import

--- src/com/steve/commands/PlayerDataCmd.java
+++ src/com/steve/commands/PlayerDataCmd.java
@@ -3,7 +3,6 @@ package com.steve.commands;
 import com.steve.Util;
 import com.steve.PlayerData;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
